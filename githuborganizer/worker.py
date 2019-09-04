99df75ac907d9ac2c28a9812d2c75de900358f75
Robert Hafner <tedivm@tedivm.com>
2019-09-04 10:01:33 -0700

    Don't run "process installs" if environmental variable is not set

--- githuborganizer/worker.py
+++ githuborganizer/worker.py
@@ -3,10 +3,11 @@ from githuborganizer.tasks import github
 from githuborganizer import celery
 import os
 
-@celery.on_after_finalize.connect
-def setup_periodic_tasks(sender, **kwargs):
-    print('Root Task - Schedule Installation Jobs')
-    sender.add_periodic_task(
-        float(os.environ.get('PROCESS_INSTALLS_INTERVAL', 30 * 60.0)),
-        github.process_installs.s(),
-        name='Root Task - Schedule Installation Jobs')
+if os.environ.get('PROCESS_INSTALLS_INTERVAL', False):
+    @celery.on_after_finalize.connect
+    def setup_periodic_tasks(sender, **kwargs):
+        print('Root Task - Schedule Installation Jobs')
+        sender.add_periodic_task(
+            float(os.environ.get('PROCESS_INSTALLS_INTERVAL', 30 * 60.0)),
+            github.process_installs.s(),
+            name='Root Task - Schedule Installation Jobs')
