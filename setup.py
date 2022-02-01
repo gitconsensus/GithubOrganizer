538d85bd0e65ec67e888f16430f1333d41c9de27
Toby Marshall <toby.marshall@global.com>
2022-02-01 11:48:17 +0000

    Updated all the dependencies that could be updated.

--- setup.py
+++ setup.py
@@ -41,14 +41,14 @@ setup(
 
   install_requires=[
     'Beaker>=1.11.0,<2',
-    'celery>=4.1,<5',
-    'click>=7.0,<8.0',
-    'cryptography>=2.1.4,<3',
-    'github3.py>=1,<2',
-    'github3apps.py>=0.2.0,<0.3',
-    'pyjwt>=1.5.3,<2',
-    'PyYAML>=5,<6',
-    'fastapi>=0.42.0',
+    'celery>=5.2.3,<6',
+    'click>=8.0.3,<9.0',
+    'cryptography>=36.0.1<37.0',
+    'github3.py>=3.0.0,<4',
+    'github3apps.py>=0.2.2,<0.3',
+    'pyjwt>=2.3.0,<3',
+    'PyYAML>=6.0,<7',
+    'fastapi>=0.73.0',
   ],
 
   extras_require={
